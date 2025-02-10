3001166b483be1f5cff199f5c06462a050025146
ibailopezdelapuente2 <ibai.lopezdelapuente@ikasle.egibide.org>
2025-02-10 13:06:10 +0100

    showall actividades a medias

--- front/src/components/user/activities/userActivity.vue
+++ front/src/components/user/activities/userActivity.vue
@@ -20,4 +20,6 @@
 
 <style scoped>
 
+    
+
 </style>
--- front/src/views/UserView.vue
+++ front/src/views/UserView.vue
@@ -1,6 +1,9 @@
 <script>
 import encabezado from "../components/layout/Header.vue"
 import pie from '../components/layout/Footer.vue';
+import filtersUser from '../components/user/filters/filtersUser.vue'
+import userActivity from '../components/user/activities/userActivity.vue'
+
 </script>
 
 <template>
@@ -10,9 +13,11 @@ import pie from '../components/layout/Footer.vue';
   <div class="row">
     <header class="col-12"> <encabezado/> </header>
   </div>
-
-  <div class="col-12">
-    <footer> <pie/> </footer>
+  <div class="row">
+    <div class="col-12"><filtersUser/></div>
+  </div>
+  <div class="row">
+    <footer class="col-12"> <pie/> </footer>
   </div>
 
   </body>
