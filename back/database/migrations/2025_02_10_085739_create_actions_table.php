f2ed394ee0cbebc0770f7b9404558f6413186031
JonGaray <jon.garay@ikasle.egibide.org>
2025-02-10 14:06:45 +0100

    cambios modelo User

--- back/database/migrations/2025_02_10_085739_create_actions_table.php
+++ back/database/migrations/2025_02_10_085739_create_actions_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->date('date_end');
             $table->integer('age');
             $table->string('languaje');
-            $table->date('start_time');
+            $table->dateTime('start_time');
             $table->integer('capacity');
             $table->integer('price');
             $table->integer('duration');
