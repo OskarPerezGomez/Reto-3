04a0bc156f3413405f12275e3f48d2f3e0ab0c90
JordiFernandezUralde <jordi.fernandez@ikasle.egibide.org>
2025-02-10 10:34:48 +0100

    componente login

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import encabezado from './components/layout/Header.vue'
-import pie from './components/layout/Footer.vue'
+
 </script>
 
 <template>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <body class="container text-center">
-
-    <div class="row">
-      <header class="col-12"> <encabezado/> </header>
-    </div>
-
-
-
-
-  <div class="col-12">
-    <footer> <pie/> </footer>
+  <div id="app" >
+    <router-view />
   </div>
-
-  </body>
-
 </template>
 
 <style scoped>
-
-
 </style>
