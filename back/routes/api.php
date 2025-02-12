c93f97aa8290c0646b15480b957a89ec9d9c9628
JordiFernandezUralde <jordi.fernandez@ikasle.egibide.org>
2025-02-12 08:49:23 +0100

    perfil 100% + editar perfil 100%

--- back/routes/api.php
+++ back/routes/api.php
@@ -24,9 +24,10 @@ Route::controller(UserController::class)->group(function () {
     Route::post('user', 'store');
     Route::get('user/all','showAll');
     Route::get('user/{user}', 'show');
-    Route::put('user/{user}/update', 'update');
+    Route::post('user/{user}/update', 'update');
     Route::delete('user/{user}/destroy', 'destroy');
     Route::post('user/join', 'join');
+    Route::get('user/{user}/actions','showOne');
 });
 
 Route::controller(CenterController::class)->group(function () {
